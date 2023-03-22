9e76441a5e40920dee69ccab9e704ea157191222
Nomynameisjames <adavaonimisi@gmail.com>
2023-03-22 20:59:51 +0100

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,16 +4,13 @@
 """
 
 def pascal_triangle(n):
-    triangle = []
-    for i in range(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
         row = [1]
-        if n <= 0:
-            return []
-        else:
-            for j in range(1, i):
-                row.append(triangle[i-1][j-1] + triangle[i-1][j])
-            row.append(1)
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
         triangle.append(row)
     return triangle
-
-
