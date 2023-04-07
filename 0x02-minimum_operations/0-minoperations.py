38bdfad8879d768a69171659984b4421f64aa12d
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-07 03:22:27 +0100

    finding minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 '''
     Objective write a method that calculates the fewest number of operations
@@ -12,10 +12,13 @@ def minOperations(n) -> int:
         Method calculates the fewest number of operations
         needed to result in exactly n H characters in the file.
     '''
-    spy = 0  # spy keeps track of the number of operations
-    if n <= 1:  # If n is impossible to achieve, return 0
+    # spy keeps track of the number of operations
+    spy = 0
+    # If n is impossible to achieve, return 0
+    if n <= 1:
         return 0
-    for i in range(2, n + 1):  # finding the factor of n
+    # find factors of n
+    for i in range(2, n + 1):
         while n % i == 0:
             spy = spy + i
             n = n // i
