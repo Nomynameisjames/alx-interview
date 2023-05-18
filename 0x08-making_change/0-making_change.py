7b2374768d4774d4d3fba34f387ea727db936d52
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-18 18:09:46 +0100

    removed import files

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,16 +19,20 @@ def makeChange(coins: list, total: int) -> int:
         determine the fewest number of coins needed to meet a given amount
         total.
     """
+
     if total <= 0:
         return 0
+
     coins.sort(reverse=True)
     num_coins = 0
+
     for coin in coins:
         if total <= 0:
             break
-        if coin <= total:
-            num_coins += total // coin
-            total = total % coin
+        num_coins += total // coin
+        total = total % coin
+
     if total != 0:
         return -1
+
     return num_coins
