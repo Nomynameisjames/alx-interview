e2c1fb3396bb71d8991f058af89401bda355f882
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-18 17:49:23 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from typing import List
+"""
+    Prototype:
+            def makeChange(coins, total)
+                    Return: fewest number of coins needed to meet total
+                    If total is 0 or less:
+                        return 0
+                    If total cannot be met by any number of coins you have:
+                        return -1
+    coins is a list of the values of the coins in your
+    value of a coin will always be an integer greater than 0
+    You can assume you have an infinite number of each denomination of coin
+    in the list
+"""
+
+
+def makeChange(coins: List, total: int) -> int:
+    """
+        determine the fewest number of coins needed to meet a given amount
+        total.
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        if coin <= total:
+            num_coins += total // coin
+            total = total % coin
+    if total != 0:
+        return -1
+    return num_coins
